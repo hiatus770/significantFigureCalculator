f08ddb392abd3e3583afbbd4e40f4083bef2e050
Hiatus <77402029+hiatus770@users.noreply.github.com>
2023-09-10 20:44:06 -0400

    Working Vers

---
+++ script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById('sigInput');
+input.addEventListener('input', function() {
+  const value = input.value;
+  console.log(value); // replace with your own code
+});
